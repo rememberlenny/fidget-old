8294ee4f4ed63642758c6e22ea7ef0a4d70e7c84
RememberLenny <leonard_bogdonoff@condenast.com>
2014-04-25 17:39:06 -0400

    Add foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MapTrackRails::Application.routes.draw do
+  get "pages/home"
+  get "pages/about"
+  get "pages/contact"
+  get "pages/download"
   resources :scaffolds
 
   # The priority is based upon order of creation: first created -> highest priority.
