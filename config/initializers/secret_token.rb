adb1334c2ec23a197db22b67608680624651c4bf
RememberLenny <leonard_bogdonoff@condenast.com>
2014-04-25 17:35:18 -0400

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapTrackRails::Application.config.secret_key_base = '561de2ac05d59f31edc92e558af11625c7d2196afde2116baaac00f56b36d8e6e17f9d792f92adc75c15b7372ac3a8ea895165356cbb940f80b1453ae45fe7cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MapTrackRails::Application.config.session_store :cookie_store, key: '_map-track-rails_session'
