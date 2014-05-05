80d1669ac010441ea31f76b725db876c5d8be9c3
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-05 15:43:26 -0400

    Create mapbox marker object

--- app/assets/javascripts/map/mapbox.js
+++ app/assets/javascripts/map/mapbox.js
@@ -1,5 +1,5 @@
-var click = document.getElementById('click'),
-    mousemove = document.getElementById('mousemove');
+// var click = document.getElementById('click'),
+//     mousemove = document.getElementById('mousemove');
 
 var mapApp = {};
 
@@ -7,40 +7,18 @@ mapApp = {
     initialize: function(){
       this.loadMap();
       this.mouseActions();
-      this.insertMarker(-74.00663, 40.714623, 'Title', 'Desc');
     },
     loadMap: function(){
-        var map = L.mapbox
+        this.map = L.mapbox
                    .map('map', 'lkbgift.i30dhib8',  { zoomControl: false })
                    .setView([40.714623, -74.00663], 16);
 
     },
     mouseActions: function(){
-        map.on('mousemove click', function(e) {
-            window[e.type].innerHTML = e.containerPoint.toString() + ', ' + e.latlng.toString();
+        this.map.on('mousemove click', function(e) {
+            console.log( e.containerPoint.toString() + ', ' + e.latlng.toString());
         });
-
-    },
-    insertMarker: function(lon, lat, title, desc){
-      L.mapbox.featureLayer({
-        // this feature is in the GeoJSON format: see geojson.org
-        // for the full specification
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          // coordinates here are in longitude, latitude order because
-          // x, y is the standard for GeoJSON and many formats
-          coordinates: [lon, lat]
-        },
-
-        properties: {
-          title: title,
-          description: desc,
-          // one can customize markers by adding simplestyle properties
-          // http://mapbox.com/developers/simplestyle/
-          'marker-size': 'large',
-          'marker-color': '#f0a'
-        }
-      }).addTo(map);
     }
 }
+
+mapApp.initialize();
---
+++ app/assets/javascripts/map/mapboxMarkers.js
@@ -0,0 +1,20 @@
+function MapboxMarker (lng, lat, title, desc){
+  var map = mapApp.map;
+  var geoData = {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [parseFloat(lng), parseFloat(lat)]
+    },
+    "properties": {
+      "title": title,
+      "description": desc,
+      "marker-color": "#fc4353",
+      "marker-size": "large",
+      "marker-symbol": "harbor"
+    }
+  };
+  console.log(geoData);
+  console.log(map);
+  L.mapbox.featureLayer(geoData).addTo(mapApp.map);
+}
