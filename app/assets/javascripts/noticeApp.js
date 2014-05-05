33d189fa66022cb1c6565a7546125820ea196e86
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-05 11:57:52 -0400

    Create notice app object and constructor

--- app/assets/javascripts/noticeApp.js
+++ app/assets/javascripts/noticeApp.js
@@ -1,31 +1,19 @@
-var noticeApp = {};
-
-noticeApp = {
-  message: {
+function Notice(title, message){
+  this.message = {
     // title
     // message
-    title: 'Notice',
-    message: 'This is a message'
-  },
-  initNotice: function(title, message){
-    this.defineMessage(title, message);
-    this.callNotice();
-  },
-  defineMessage: function(title, message){
-    this.message.title = title;
-    this.message.message = message;
-  },
-  callNotice: function(){
-    this.appendNotice();
-    $('.panel.notice').addClass('visible');
-  },
-  appendNotice: function(){
+    title: title,
+    message: message
+  };
+  this.createNotice = function(){
     var context = this.message
     var template = HandlebarsTemplates['notices/notice'](context);
     $('#notice-wrap').append(template);
-  },
-  closeNotice: function(){
+    $('.panel.notice').addClass('visible');
+  };
+  this.closeNotice = function(){
     $('.panel.notice').removeClass('visible');
-  },
+  };
+  this.createNotice();
 }
 
