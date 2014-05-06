674e7f7f07515133286cdae463637ecd9f4127cc
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-06 09:32:09 -0400

    Prevent assets from precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 MapTrackRails::Application.configure do
-
+  config.serve_static_assets = true
   config.assets.initialize_on_precompile = false
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -21,9 +21,6 @@ MapTrackRails::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
