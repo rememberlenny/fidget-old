9c2a6e69dbc6683b86bb2335bbada3647e58db20
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-17 23:33:53 -0400

    Rake db migrate the scaffolded location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140425213628) do
+ActiveRecord::Schema.define(version: 20140518033226) do
+
+  create_table "locations", force: true do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "scaffolds", force: true do |t|
     t.string   "word"
