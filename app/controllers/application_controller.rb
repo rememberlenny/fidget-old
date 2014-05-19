3ef15d50fe37807fc6c579e0933782e0b9582297
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 06:52:44 -0400

    Create the configure devise param for application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  protected
+
+  before_filter :configure_devise_params, if: :devise_controller?
+    def configure_devise_params
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :name, :email, :password, :password_confirmation, :current_password  )}
+    end
+
 end
