c8bf2a22cc0e8259f9e42ffe4e6a41e4610d6703
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 13:04:34 -0400

    Silence all relationships. Recreate later

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,6 @@ class Location < ActiveRecord::Base
   belongs_to :project
   geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
-   validates :project_id, presence: true
+  # validates :project_id, presence: true
   after_validation :reverse_geocode, :geocode, :if => :address_changed?
 end
--- app/models/map_projects.rb
+++ app/models/map_projects.rb
@@ -1,6 +1,6 @@
 class MapProjects < ActiveRecord::Base
-  belongs_to :user
-  has_many :locations
-  default_scrop -> { order('created_at DESC') }
-  validates :user_id, presence: true
+  # belongs_to :user
+  # has_many :locations
+  # default_scrop -> { order('created_at DESC') }
+  # validates :user_id, presence: true
 end
