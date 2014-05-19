4b91aeb52b3b047f6f04081a78c2bbb60b0ba6c7
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 07:15:32 -0400

    Set up the devise routes for omniauth facebook'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 MapTrackRails::Application.routes.draw do
-  devise_for :users
+  devise_for :users,  path_names: {sign_in: "login", sign_out: "logout"},
+                      controllers: {
+                        :omniauth_callbacks => "omniauth_callbacks"
+                      }
+
   resources :users
 
   resources :locations
