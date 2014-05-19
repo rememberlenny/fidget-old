39e6ae38e739819eb87c7cfe5f3b404405671af2
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 07:02:46 -0400

    Silence the segment class because missing actual secret

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,5 +1,5 @@
-Analytics = AnalyticsRuby       # Alias for convenience
-Analytics.init({
-    secret: ENV['SEGMENT_IO_MAPMADE'],          # The write key for atriangle/atriangle
-    on_error: Proc.new { |status, msg| print msg }  # Optional error handler
-})
+# Analytics = AnalyticsRuby       # Alias for convenience
+# Analytics.init({
+#     secret: ENV['SEGMENT_IO_MAPMADE'],          # The write key for atriangle/atriangle
+#     on_error: Proc.new { |status, msg| print msg }  # Optional error handler
+# })
