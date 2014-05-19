60a104bbb105c87bdbea198c08b6f5411d356996
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 07:03:14 -0400

    migrate the images onto users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519102155) do
+ActiveRecord::Schema.define(version: 20140519105406) do
 
   create_table "locations", force: true do |t|
     t.string   "address"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140519102155) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
