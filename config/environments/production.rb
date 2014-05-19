5acade52aa1a9d6b49c0145471b2d9d3aa7cbf5c
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 06:23:15 -0400

    Change the mailer on the prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ MapTrackRails::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'www.mapmade.io' }
+  config.action_mailer.default_url_options = { :host => 'mapmade.io' }
 end
