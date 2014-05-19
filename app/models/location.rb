cd0cc2a9659fddc95b13eea8b764b80e25c6e5cf
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 07:57:22 -0400

    Create the user<-project<-location relationships

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
 class Location < ActiveRecord::Base
+  belongs_to :project
   geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
+   validates :project_id, presence: true
   after_validation :reverse_geocode, :geocode, :if => :address_changed?
 end
