885f0e8cf102b0c3d6b34a53904e0fe2ebb853d7
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-19 06:20:34 -0400

    Generate devise views

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.assets.initialize_on_precompile = false
+
 module MapTrackRails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,5 @@ MapTrackRails::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
