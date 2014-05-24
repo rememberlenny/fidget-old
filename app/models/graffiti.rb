4092dae22f5511e1f757706568c5283e2c0555d5
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-24 15:04:40 -0400

    Save the updated attr-accessible

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -1,12 +1,13 @@
 class Graffiti < ActiveRecord::Base
 
   def self.import(file)
+    allowed_attributes = [ "id","image_id","image_url","user","date_published","post_id","name","released_on","price","created_at","updated_at"]
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
       product = find_by_id(row["id"]) || new
-      product.attributes = row.to_hash.slice(*accessible_attributes)
+      product.attributes = row.to_hash.select { |k,v| allowed_attributes.include? k }
       product.save!
     end
   end
