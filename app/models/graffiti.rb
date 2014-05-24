9e877080f81079e5d80f285166a5050a76797d19
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-24 14:52:14 -0400

    Save the Roo gem

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -13,9 +13,9 @@ class Graffiti < ActiveRecord::Base
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-    when ".csv" then Csv.new(file.path, nil, :ignore)
-    when ".xls" then Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+    when ".csv" then Roo.new(file.path, nil, :ignore)
+    when ".xls" then Roo.new(file.path, nil, :ignore)
+    when ".xlsx" then Roo.new(file.path, nil, :ignore)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
