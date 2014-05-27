604b16e83e29da8fffd8cd29e8450ee4fdd35ab4
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-27 14:46:34 -0400

    Corrected the '\r\n'comma issue

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -1,7 +1,7 @@
 class Graffiti < ActiveRecord::Base
   def self.import(file)
     @file = file.tempfile
-    SmarterCSV.process(@file, :chunk_size => 50000) do |chunk|
+    SmarterCSV.process(@file, {:chunk_size => 50000, :col_sep => "@", :quote_char => "\x00"}) do |chunk|
       chunk.each do |row|
         row = row.slice(:image_id, :image_url, :user,  :date_published,  :post_id)
         row.values.each { |f| f.gsub!(/\"/, '') }
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'csv'
 require 'smarter_csv'
 require 'iconv'
 
