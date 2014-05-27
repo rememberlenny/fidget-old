f123456cb3d3b1d1662a69833220ea7d8640ea97
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-27 15:55:54 -0400

    Spacing

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -2,12 +2,12 @@ class Graffiti < ActiveRecord::Base
   def self.import(file)
     @file = file.tempfile
     SmarterCSV.process(@file, {:chunk_size => 100, :quote_char => "\x00"}, :key_mapping => {
-      :image_id =>:image_id,
-      :image_url=>:image_url,
-      :user=>:user,
-      :source=>'12ozprophet',
-      :date_published=>:date_published,
-      :post_id=>:post_id
+        :image_id       =>:image_id,
+        :image_url      =>:image_url,
+        :user           =>:user,
+        :source         =>'12ozprophet',
+        :date_published =>:date_published,
+        :post_id        =>:post_id
       }) do |chunk|
       chunk.each do |row|
         Graffiti.create(row)
