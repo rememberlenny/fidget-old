858a4749c103d09269c6c818a324caec61430a3a
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-27 15:31:32 -0400

    Change the batch size for memory purposes

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ end
 
 gem "rmagick"
 gem "carrierwave"
-gem "resque", "~> 2.0.0.pre.1", github: "resque/resque"
+# gem "resque", "~> 2.0.0.pre.1", github: "resque/resque"
 gem 'geocoder'
 gem 'roo'
 gem 'smarter_csv'
--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -1,7 +1,7 @@
 class Graffiti < ActiveRecord::Base
   def self.import(file)
     @file = file.tempfile
-    SmarterCSV.process(@file, {:chunk_size => 50000, :quote_char => "\x00"}) do |chunk|
+    SmarterCSV.process(@file, {:chunk_size => 5000, :quote_char => "\x00"}) do |chunk|
       chunk.each do |row|
         row = row.slice(:image_id, :image_url, :user,  :date_published,  :post_id)
         Graffiti.create(row)
