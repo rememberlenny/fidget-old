9dcde56896caa6f5e3c336921793ef238d34cc4d
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-27 16:05:19 -0400

    Correct the params on the graffitis

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -1,14 +1,14 @@
 class Graffiti < ActiveRecord::Base
   def self.import(file)
     @file = file.tempfile
-    SmarterCSV.process(@file, {:chunk_size => 100, :quote_char => "\x00"}, :key_mapping => {
+    SmarterCSV.process(@file, {:chunk_size => 100, :quote_char => "\x00", :key_mapping => {
         :image_id       =>:image_id,
         :image_url      =>:image_url,
         :user           =>:user,
         :source         =>'12ozprophet',
         :date_published =>:date_published,
         :post_id        =>:post_id
-      }) do |chunk|
+      }}) do |chunk|
       chunk.each do |row|
         Graffiti.create(row)
       end
