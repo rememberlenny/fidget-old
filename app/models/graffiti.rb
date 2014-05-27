0391505e7b82c467bb05f268714b4a96101a573f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-27 14:56:47 -0400

    Correct the quotation definer

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -1,10 +1,9 @@
 class Graffiti < ActiveRecord::Base
   def self.import(file)
     @file = file.tempfile
-    SmarterCSV.process(@file, {:chunk_size => 50000, :col_sep => "@", :quote_char => "\x00"}) do |chunk|
+    SmarterCSV.process(@file, {:chunk_size => 50000, :quote_char => "\x00"}) do |chunk|
       chunk.each do |row|
         row = row.slice(:image_id, :image_url, :user,  :date_published,  :post_id)
-        row.values.each { |f| f.gsub!(/\"/, '') }
         Graffiti.create(row)
       end
     end
