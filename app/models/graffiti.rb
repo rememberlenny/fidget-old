3353110dbe00504bcc340d358fa4a4a0dd7672af
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-27 10:32:10 -0400

    Update the import statement

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -1,7 +1,7 @@
 class Graffiti < ActiveRecord::Base
 
   def self.import(file)
-    allowed_attributes = [ "id","image_id","image_url","user","date_published","post_id","name","released_on","price","created_at","updated_at"]
+    allowed_attributes = [ "image_id", "image_url", "user",  "date_published",  "post_id"]
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
@@ -14,9 +14,9 @@ class Graffiti < ActiveRecord::Base
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-    when ".csv" then Roo::CSV.new(file.path, nil, :ignore)
-    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+    when ".csv" then Roo::CSV.new(file.path)
+    when ".xls" then Roo::Excel.new(file.path)
+    when ".xlsx" then Roo::Excelx.new(file.path)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
