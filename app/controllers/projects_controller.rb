b176deb4e06a12ce3e9a9b092bc786c802453969
RememberLenny <leonard_bogdonoff@condenast.com>
2014-06-27 17:01:32 -0400

    Do reverse and geocoding on project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -75,7 +75,7 @@ class ProjectsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_project
+    def set_location
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :locations
+    geocoded_by :address
+  reverse_geocoded_by :latitude, :longitude
+  # validates :project_id, presence: true
+  after_validation :reverse_geocode, :geocode, :if => :address_changed?
   # default_scrop -> { order('created_at DESC') }
   # validates :user_id, presence: true
 end
