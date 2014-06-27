80bc7bced6e291ffaa1d10667d16ebf370af273f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-06-27 17:04:36 -0400

    Correct error in 'set_project'

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -75,7 +75,9 @@ class ProjectsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_location
+    def set_project
+      @user     = current_user
+      @project  = @user.projects.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
